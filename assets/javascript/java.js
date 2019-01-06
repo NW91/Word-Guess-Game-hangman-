c47a1361ea995f0f8a8b4f4fd1adc5388be77a47
Nicole W <nicoleewhitee@gmail.com>
2019-01-05 21:35:51 -0800

    css styling

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -2,8 +2,8 @@
 var wordOptions = ["seattle", "portland", "bellingham", "spokane", "tacoma", "beaverton", "olympia", "salem", "bend"];
 var selectedWord = "";
 var lettersInWord = [];
-var numBlanks = 0;
-var blanksAndSuccesses = [];
+var numberOfBlanks = 0;
+var blanksPlusCorrects = [];
 var wrongGuesses = [];
 
 // Game Counter
@@ -12,25 +12,24 @@ var lossCount = 0;
 var guessesRemaining = 0;
 
 // Functions
-
 function startGame() {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
     lettersInWord = selectedWord.split("");
-    numBlanks = lettersInWord.length;
+    numberOfBlanks = lettersInWord.length;
 
     // reset
     guessesRemaining = 15;
     wrongLetters = [];
-    blanksAndSuccesses = [];
+    blanksPlusCorrects = [];
 
     // makes the right amount of blanks for each word
-    for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("_");
+    for (var i = 0; i < numberOfBlanks; i++) {
+        blanksPlusCorrects.push("_");
     }
 
 
     // change html to show which game number it is in the counter
-    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wordToGuess").innerHTML = blanksPlusCorrects.join(" ");
     document.getElementById("numGuesses").innerHTML = guessesRemaining;
     document.getElementById("winCounter").innerHTML = winCount;
     document.getElementById("lossCounter").innerHTML = lossCount;
@@ -38,8 +37,8 @@ function startGame() {
     // Testing/Debugging
     console.log(selectedWord);
     console.log("letters in word " + lettersInWord);
-    console.log("num blanks " + numBlanks);
-    // console.log("blanks and successes " + blanksAndSuccesses);
+    console.log("num blanks " + numberOfBlanks);
+    // console.log("blanks and successes " + blanksPlusCorrects);
 
 }
 
@@ -47,7 +46,7 @@ function checkLetters(letter) {
     // check if the letter exists anywhere in the word
     var isLetterInWord = false;
 
-    for (var i = 0; i < numBlanks; i++) {
+    for (var i = 0; i < numberOfBlanks; i++) {
         if (selectedWord[i] == letter) {
             isLetterInWord = true;
         }
@@ -55,9 +54,9 @@ function checkLetters(letter) {
 
     // check to find if the letter fits in the word, and then notate 
     if (isLetterInWord) {
-        for (var i = 0; i < numBlanks; i++) {
+        for (var i = 0; i < numberOfBlanks; i++) {
             if (selectedWord[i] == letter) {
-                blanksAndSuccesses[i] = letter;
+                blanksPlusCorrects[i] = letter;
             }
         }
     }
@@ -70,19 +69,19 @@ function checkLetters(letter) {
 }
 
 // keeps characters guesses to only letters
-function isValidGuess(lettersInWord) {
-    return /^[A-Za-z]$/.test(wrongGuesses);
+function isValidGuess(wordToGuess) {
+    return /^[A-Za-z]$/.test();
 }
 
 function roundComplete() {
     console.log("Win count: " + winCount + " | Loss count: " + lossCount + " | guesses left: " + numGuesses);
 
     document.getElementById("numGuesses").innerHTML = guessesRemaining;
-    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wordToGuess").innerHTML = blanksPlusCorrects.join(" ");
     document.getElementById("wrongLetters").innerHTML = wrongLetters.join(" ");
 
 
-    if(lettersInWord.toString() == blanksAndSuccesses.toLocaleString()){
+    if(lettersInWord.toString() == blanksPlusCorrects.toLocaleString()){
         winCount++;
         alert("Congratulations, you've won this round!");
 
